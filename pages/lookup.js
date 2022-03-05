573298083aba628ffe87caab4acc771ad59ede12
Nytroid <contactdevanshu2007@gmail.com>
2022-03-05 14:02:18 -0600

    animated

--- pages/lookup.js
+++ pages/lookup.js
@@ -10,6 +10,9 @@ import FormControl from '@mui/material/FormControl';
 import { useRadioGroup } from '@mui/material';
 import { createTheme} from '@mui/material/styles';
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import Grid from '@mui/material/Grid';
+import Bounce from 'react-reveal/Bounce'; 
+import { Reveal } from 'react-reveal';
 
 
 const theme = createTheme({
@@ -32,7 +35,6 @@ const CustomTheme = createTheme({
               color: "#73ff7a"}}]}}});
 
 export default function Weather() {
-
     const APIkey = "a0aacf0e9a9faab5cbb37f243e0f7f94"
 
 
@@ -46,6 +48,12 @@ export default function Weather() {
         symbol: '°F',
         Celsius: 0,
         Farenheit: 0,
+        date: 0,
+        TwoHourCelsius: 'None',
+        TwoHourFarenheit: 'None',
+        FeelsLikeCelsius: 0,
+        FeelsLikeFarenheit: 0,
+        showTemp: true,
     }    
 
     const [state, setState] = useState(initialState)
@@ -75,28 +83,46 @@ export default function Weather() {
 
     const getWeather = async () => {
         try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${state.lat}&lon=${state.long}&appid=${APIkey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${state.lat}&lon=${state.long}&exclude=minutely,daily&appid=${APIkey}`)
         const data = await response.json()
-        if (data.cod == 401) {setState({...state,
-          Farenheit: 'None',
-          Celsius: 'None'}) 
-        return;
-    }
-          let Farenheit = 9/5*(data.main.temp-273) + 32
-          let Celsius = data.main.temp - 273.5
-          const F = Farenheit.toFixed(2)
-          const C = Celsius.toFixed(2)
+          let Farenheit = 9/5*(data.current.temp-273) + 32
+          let Celsius = data.current.temp - 273.5
+          const F = Farenheit.toFixed(0)
+          const C = Celsius.toFixed(0)
+          let FeelsLikeFarenheit = 9/5*(data.current.feels_like-273) + 32
+          let FeelsLikeCelsius = data.current.feels_like - 273.5
+          const FF = FeelsLikeFarenheit.toFixed(0)
+          const FC = FeelsLikeCelsius.toFixed(0)
+          const myDate = new Date(data.hourly[2].dt*1000);
+          const TwoHourTemp = data.hourly[2].temp
+          const TwoHourFeelsLike = data.hourly[2].feels_like
+          const FarenheitHour = 9/5*(TwoHourTemp-273) + 32
+          const CelsiusHour = TwoHourTemp - 273.5
+          const FH = FarenheitHour.toFixed(0)
+          const CH = CelsiusHour.toFixed(0)   
+
+          const FeelsLikeFarenheitHour = 9/5*(TwoHourFeelsLike-273) + 32
+          const FeelsLikeCelsiusHour = TwoHourFeelsLike - 273.5
+          const FFH = FeelsLikeFarenheitHour.toFixed(0)
+          const FCH = FeelsLikeCelsiusHour.toFixed(0)   
           setState({...state,
+            date: myDate,
           Celsius: C,
-          Farenheit: F
+          Farenheit: F,
+          TwoHourCelsius: CH,
+          TwoHourFarenheit: FH,
+          FeelsLikeFarenheit: FF,
+          FeelsLikeCelsius: FC,
+          FeelsLikeFarenheitHour: FFH,
+          FeelsLikeCelsiusHour: FCH,
       })}
         catch(err){
           if (err == "TypeError: Cannot read properties of undefined (reading 'temp')") {
             setState({...state,
             Farenheit: 'None',
-          Celsius: 'None'})
+          Celsius: 'None'})} else {
+          window.alert(err)}
             }
-        }
 
         }
 
@@ -143,16 +169,22 @@ export default function Weather() {
 
     const scrollDown = () => {
       divRef.current.scrollIntoView({ behavior: 'smooth' });
+      setState({...state,
+        showTemp: true})
     }
 
     const scrollUp = () => {
       mainRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
+      setState({...state,
+        showTemp: false})
+      }
 
     const renderTemp = () => {
         let temperature = 0
+        let FeelsLike
 
         state.symbol == '°F' ? temperature = state.Farenheit : temperature = state.Celsius
+        state.symbol == '°F' ? FeelsLike = state.FeelsLikeFarenheit : FeelsLike = state.FeelsLikeCelsius
         if (state.Farenheit == 'None' && state.Celsius == 'None') return (
           <>
           City not found
@@ -175,8 +207,8 @@ export default function Weather() {
         return ( 
           <>
           <div className={styles.Weathercard}>
-            <p className={styles.code}>The temperature is</p>
-            <h1> {temperature}{state.symbol}</h1>
+            <h1>Temperature: {temperature}{state.symbol}</h1>
+            <h1>Feels Like: {FeelsLike}{state.symbol}</h1>
             <p className={styles.code}> In {state.city}, {state.state} </p> 
           
         </div>
@@ -205,13 +237,34 @@ export default function Weather() {
       </>
         )
     }
+
+    const renderHourlyWeather = () => {
+      let date = state.date
+      let today = new Date()
+      let difference = Math.abs(date - today)
+      let diffHrs = Math.ceil(difference / (1000 * 60 * 60))
+      let hour = 'hour'
+      let temp = state.TwoHourCelsius
+      let FeelsLike = state.FeelsLikeCelsiusHour
+      if (diffHrs > 1) hour = 'hours'
+      if (state.symbol == '°F') {temp = state.TwoHourFarenheit}
+      if (state.symbol == '°F') {FeelsLike = state.FeelsLikeFarenheitHour}
+      return (
+      <>
+      <div className={styles.Weathercard}>
+      <p className={styles.code}>The temperature will be</p>
+      <h1> {temp}{state.symbol}</h1>
+      <p className={styles.code}>and</p>
+      <h1>Feel Like: {FeelsLike}{state.symbol}</h1>
+      <p className={styles.code}> In {diffHrs} {hour} </p> 
+      </div>
+      </>
+      )}
     
     return (
         <>           
-
 <main className={styles.main} ref={mainRef}>
         <a className={styles.card}>
-        
         <form 
         id='cityForm' 
         onSubmit={getCoordinatesForm} 
@@ -237,8 +290,22 @@ export default function Weather() {
         </a> 
     </main>
       <div ref={divRef}>
+        
     <main className={styles.main} align="center">
-    <h2>{renderTemp()}</h2>
+      
+          <Bounce left opposite when={state.showTemp} delay={0}>
+          <Grid container
+          zeroMinWidth
+            direction='row'
+            justifyContent='space-evenly'>
+            <Grid item>
+              {renderHourlyWeather()}
+            </Grid>
+          <Grid item>{renderTemp()}</Grid>
+          <Grid item>{renderHourlyWeather()}</Grid>
+        </Grid>
+  </Bounce>
+    
     </main>
     </div>
     </>
